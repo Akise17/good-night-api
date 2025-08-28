2a82db8402af2580bed04fa26d45fd0bdbc2f571
Akise17 <ahmad.fauzan1603@gmail.com>
2025-08-28 17:22:36 +0700

    add test for tracking index by follower

--- test/controllers/api/v1/follows_controller_test.rb
+++ test/controllers/api/v1/follows_controller_test.rb
@@ -31,4 +31,26 @@ class Api::V1::FollowsControllerTest < ActionDispatch::IntegrationTest
     assert_response :no_content
     assert_not_includes @user.followings.reload, @other_user
   end
+
+  test 'by_followers returns only followings trackings' do
+    @follower = @other_user
+    @non_follower = users(:andy)
+
+    @follower.follow(@user)
+
+    @follower_tracking = Tracking.create!(user: @follower, clock_in_time: 2.hours.ago, clock_out_time: 1.hour.ago)
+    @non_follower_tracking = Tracking.create!(user: @non_follower, clock_in_time: 3.hours.ago, clock_out_time: 2.hours.ago)
+
+    @user.follow(@follower)
+
+    get by_followers_api_v1_trackings_url(date_range: Date.today.to_s), headers: auth_headers, as: :json
+
+    assert_response :success
+
+    body = JSON.parse(response.body)
+    user_ids = body['data'].map { |t| t['user']['id'] }
+
+    assert_includes user_ids, @follower.id
+    assert_not_includes user_ids, @non_follower.id
+  end
 end
